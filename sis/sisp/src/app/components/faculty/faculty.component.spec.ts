20e665e7d34eb89a5167c5a8d295e3180588db42
ashwini168 <guddadamaniashwini@gmail.com>
2024-01-10 20:53:39 +0530

    changed components

--- sis/sisp/src/app/components/faculty/faculty.component.spec.ts
+++ sis/sisp/src/app/components/faculty/faculty.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ComponentsComponent } from './components.component';
+import { FacultyComponent } from './faculty.component';
 
-describe('ComponentsComponent', () => {
-  let component: ComponentsComponent;
-  let fixture: ComponentFixture<ComponentsComponent>;
+describe('FacultyComponent', () => {
+  let component: FacultyComponent;
+  let fixture: ComponentFixture<FacultyComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ComponentsComponent]
+      declarations: [FacultyComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(ComponentsComponent);
+    fixture = TestBed.createComponent(FacultyComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
